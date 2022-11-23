70d876e88a9b9b21c0b8a4397ce831a100fa87c2
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-11-23 11:14:07 +0100

    Add my Name

--- src/FeatureB.java
+++ src/FeatureB.java
@@ -1,6 +1,6 @@
 public class FeatureB {
 
     public static void main(String[] args) {
-        System.out.println("Das ist Feature B von: ");
+        System.out.println("Das ist Feature B von: Julia");
     }
 }
