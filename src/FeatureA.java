df717b818fab6ca83926acfdca4a5485619d12ac
martinkascak <m.kascak93@gmail.com>
2022-11-23 11:09:56 +0100

    Add Name

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -3,6 +3,6 @@
 public class FeatureA{
 
     public static void main(String[] args) {
-        System.out.println("Hallo, das ist der Code von <Name>");
+        System.out.println("Hallo, das ist der Code von Martin");
     }
 }
